3af6b688ae749b20b4a8b4621d6a78262909b38c
Ascale Carla Beghin <cbeghin@wustl.edu>
2018-11-28 12:46:56 -0600

    exercise detail view swift file added + strybrd mods

---
+++ iFlex-WORKING/ExerciseDetailVieew.swift
@@ -0,0 +1,35 @@
+//
+//  ExerciseDetailVieew.swift
+//  iFlex-WORKING
+//
+//  Created by Carla Beghin on 11/28/18.
+//  Copyright © 2018 Kendra Swanson. All rights reserved.
+//
+
+import UIKit
+
+class ExerciseDetailVieew: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
