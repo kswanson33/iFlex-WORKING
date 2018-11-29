de7d98ce4f2f900e88229ac29784fcf4e6632ba3
Ascale Carla Beghin <cbeghin@wustl.edu>
2018-11-29 16:40:10 -0600

    Exercise detail view done (no constraints, unsure if it looks aight)

--- iFlex-WORKING/ExerciseVC.swift
+++ iFlex-WORKING/ExerciseVC.swift
@@ -13,14 +13,28 @@ class ExerciseVC: UIViewController{
     
     var exercise: MyExercise?
     
+    @IBOutlet weak var desc: UILabel!
     @IBOutlet var label: UILabel!
     @IBOutlet var iconImage: UIImageView!
+    @IBOutlet weak var mistakes: UILabel!
+    @IBOutlet weak var equip: UILabel!
+    @IBOutlet weak var dif: UILabel!
+    @IBOutlet weak var mc: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         if let e = exercise {
             label.text = e.exercise.name
             iconImage.image = exerciseEnumToIcon(area: e.exercise.icon)
+            desc.text = String(e.exercise.directions)
+            mistakes.text = e.exercise.commonMistakes
+            for eq in e.exercise.equipment {
+                equip.text?.append(eq + " ")
+            }
+            dif.text = String(e.exercise.difficulty)
+            for mc in e.exercise.muscleGroups {
+                equip.text?.append(mc + " ")
+            }
         }
     }
     
